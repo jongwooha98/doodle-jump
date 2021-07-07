4f140dd4fe01dcdad066a9d547cf502457ed3f3b
JongWoo Ha <jongwooha98@gmail.com>
2021-07-08 02:14:55 +0900

    Update app.js
    - doodler now doesn't get stuck at the edge of the screen, but rather appears at the other side of the screen
    - displays score when game over
    - change of unit (rem to px)
    - detects if device is whether 'mobile or tablet' or not

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
+  const gridHeight = document.querySelector('.grid').offsetHeight;
   const doodler = document.createElement('div');
-
-  let doodlerLeftSpace = 5;
-  let startPoint = 15;
+  let doodlerLeftSpace = 50;
+  let startPoint = 150;
   let doodlerBottomSpace = startPoint;
   let isGameOver = false;
-  let platformCount = 5;
+  let platformCount = gridHeight / 120;
   let platforms = [];
   let upTimerId;
   let downTimerId;
@@ -17,62 +17,63 @@ document.addEventListener('DOMContentLoaded', () => {
   let rightTimerId;
   let score = 0;
 
-  function createDoodler() {
-    grid.appendChild(doodler);
-    doodler.classList.add('doodler');
-    doodlerLeftSpace = platforms[0].left;
-    doodler.style.left = doodlerLeftSpace + 'rem';
-    doodler.style.bottom = doodlerBottomSpace + 'rem';
-  }
-
+  // create platforms
   class Platform {
     constructor(newPlatformBottom) {
       this.bottom = newPlatformBottom;
-      this.left = Math.random() * 31.5;
+      this.left = Math.random() * 315;
       this.visual = document.createElement('div');
-
       const visual = this.visual;
       visual.classList.add('platform');
-      visual.style.left = this.left + 'rem';
-      visual.style.bottom = this.bottom + 'rem';
+      visual.style.left = this.left + 'px';
+      visual.style.bottom = this.bottom + 'px';
       grid.appendChild(visual);
     }
   }
   function createPlatforms() {
     for (let i = 0; i < platformCount; i++) {
-      let platformGap = 60 / platformCount;
-      let newPlatformBottom = 10 + i * platformGap;
+      let platformGap = gridHeight / platformCount;
+      let newPlatformBottom = 100 + i * platformGap;
       let newPlatform = new Platform(newPlatformBottom);
       platforms.push(newPlatform);
       console.log(platforms);
     }
   }
+  // create doodler
+  function createDoodler() {
+    grid.appendChild(doodler);
+    doodler.classList.add('doodler');
+    doodlerLeftSpace = platforms[0].left;
+    doodler.style.left = doodlerLeftSpace + 'px';
+    doodler.style.bottom = doodlerBottomSpace + 'px';
+  }
+  // move platforms
   function movePlatforms() {
-    if (doodlerBottomSpace > 20) {
+    if (doodlerBottomSpace > 200) {
       platforms.forEach((platform) => {
-        platform.bottom -= 0.4;
+        platform.bottom -= 4;
         let visual = platform.visual;
-        visual.style.bottom = platform.bottom + 'rem';
-
-        if (platform.bottom < 1) {
+        visual.style.bottom = platform.bottom + 'px';
+        if (platform.bottom < 10) {
           let firstPlatform = platforms[0].visual;
           firstPlatform.classList.remove('platform');
           platforms.shift();
           score++;
-          let newPlatform = new Platform(60);
+          let newPlatform = new Platform(600);
           platforms.push(newPlatform);
         }
       });
     }
   }
+  // doodler actions
   function jump() {
     clearInterval(downTimerId);
     isJumping = true;
     upTimerId = setInterval(() => {
-      doodlerBottomSpace += 2;
-      doodler.style.bottom = doodlerBottomSpace + 'rem';
-      console.log(doodler);
-      if (doodlerBottomSpace > startPoint + 20) {
+      doodlerBottomSpace += 20;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
+      // console.log(doodler);
+      if (doodlerBottomSpace > startPoint + 200) {
         fall();
       }
     }, 30);
@@ -81,40 +82,27 @@ document.addEventListener('DOMContentLoaded', () => {
     clearInterval(upTimerId);
     isJumping = false;
     downTimerId = setInterval(() => {
-      doodlerBottomSpace -= 0.5;
-      doodler.style.bottom = doodlerBottomSpace + 'rem';
+      doodlerBottomSpace -= 5;
+      doodler.style.bottom = doodlerBottomSpace + 'px';
       if (doodlerBottomSpace <= 0) {
         gameOver();
       }
       platforms.forEach((platform) => {
         if (
           doodlerBottomSpace >= platform.bottom &&
-          doodlerBottomSpace <= platform.bottom + 1.5 &&
-          doodlerLeftSpace + 6 >= platform.left &&
-          doodlerLeftSpace <= platform.left + 8.5 &&
+          doodlerBottomSpace <= platform.bottom + 15 &&
+          doodlerLeftSpace + 60 >= platform.left &&
+          doodlerLeftSpace <= platform.left + 85 &&
           !isJumping
         ) {
-          console.log('landed');
+          // console.log('landed');
           startPoint = doodlerBottomSpace;
           jump();
         }
       });
     }, 30);
   }
-
-  function gameOver() {
-    console.log('GAMEOVER');
-    isGameOver = true;
-    while (grid.firstChild) {
-      grid.removeChild(grid.firstChild);
-    }
-    grid.innerHTML = score;
-    clearInterval(upTimerId);
-    clearInterval(downTimerId);
-    clearInterval(leftTimerId);
-    clearInterval(rightTimerId);
-  }
-
+  // doodler movements
   function control(e) {
     if (e.key == 'ArrowLeft') {
       moveLeft();
@@ -125,7 +113,6 @@ document.addEventListener('DOMContentLoaded', () => {
       moveStraight();
     }
   }
-
   function moveLeft() {
     if (isGoingRight) {
       clearInterval(rightTimerId);
@@ -134,10 +121,13 @@ document.addEventListener('DOMContentLoaded', () => {
     isGoingLeft = true;
     leftTimerId = setInterval(() => {
       if (doodlerLeftSpace >= 0) {
-        console.log('going left');
-        doodlerLeftSpace -= 0.5;
-        doodler.style.left = doodlerLeftSpace + 'rem';
-      } else moveRight();
+        // console.log('going left' + doodlerLeftSpace);
+        doodlerLeftSpace -= 5;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      } else {
+        doodlerLeftSpace = 360;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      }
     }, 30);
   }
   function moveRight() {
@@ -147,11 +137,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     isGoingRight = true;
     rightTimerId = setInterval(() => {
-      if (doodlerLeftSpace <= 36) {
-        console.log('going right');
-
-        doodlerLeftSpace += 0.5;
-        doodler.style.left = doodlerLeftSpace + 'rem';
+      if (doodlerLeftSpace <= 360) {
+        // console.log('going right' + doodlerLeftSpace);
+        doodlerLeftSpace += 5;
+        doodler.style.left = doodlerLeftSpace + 'px';
+      } else {
+        doodlerLeftSpace = 0;
+        doodler.style.left = doodlerLeftSpace + 'px';
       }
     }, 30);
   }
@@ -161,16 +153,47 @@ document.addEventListener('DOMContentLoaded', () => {
     clearInterval(leftTimerId);
     clearInterval(rightTimerId);
   }
+  // game operations
   function start() {
     if (!isGameOver) {
       createPlatforms();
       createDoodler();
       setInterval(movePlatforms, 30);
-      jump();
-      document.addEventListener('keyup', control);
+      jump(startPoint);
+      document.addEventListener('keydown', control);
     }
   }
-
+  function gameOver() {
+    console.log('GAMEOVER');
+    isGameOver = true;
+    while (grid.firstChild) {
+      grid.removeChild(grid.firstChild);
+    }
+    grid.innerHTML = 'Game Over!';
+    grid.innerHTML = 'Score: ' + score;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+    clearInterval(leftTimerId);
+    clearInterval(rightTimerId);
+    document.removeEventListener('keyup', control);
+  }
   // attact button to start
-  start();
+  let isMobile = false;
+  window.mobileAndTabletCheck = function () {
+    (function (a) {
+      if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+          a
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          a.substr(0, 4)
+        )
+      )
+        isMobile = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return isMobile;
+  };
+  if (!isMobile) {
+    start();
+  }
 });
